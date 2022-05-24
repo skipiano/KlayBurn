50313e025346c80bee1def440b35bb130208f6ce
Michael Hahn <skipiano@gmail.com>
2022-05-24 13:56:10 -0700

    fixed bug with dates, tested

--- main.py
+++ main.py
@@ -11,12 +11,18 @@ if __name__ == "__main__":
     f.close()
     start_date = datetime.datetime.strptime(last_updated_date, "%Y-%m-%d")
     kst_time_now = datetime.datetime.now(timezone("Asia/Seoul"))
-    end_date = kst_time_now - datetime.timedelta(days=1)
+    end_date = datetime.datetime(
+        kst_time_now.year, kst_time_now.month, kst_time_now.day) - datetime.timedelta(days=1)
     f = open("last_updated_date.txt", "w")
     f.write(datetime.datetime.strftime(kst_time_now, "%Y-%m-%d"))
     f.close()
     start_month = datetime.datetime(start_date.year, start_date.month, 1)
     end_month = datetime.datetime(end_date.year, end_date.month, 1)
+    # debug
+    print(start_date)
+    print(end_date)
+    print(start_month)
+    print(end_month)
 
     # response = requests.get(
     #     "https://klaytn-proposed-block.s3.ap-northeast-2.amazonaws.com/cypress/2022Jan/ProposedBlocks_2022Jan_0xed6ee8a1877f9582858dbe2509abb0ac33e5f24e.csv")
