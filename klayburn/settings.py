0392badabc2f05cd2e244e60342f96d92b5dad47
Michael Hahn <skipiano@gmail.com>
2022-06-02 17:58:29 -0700

    update allowed_host

--- klayburn/settings.py
+++ klayburn/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get(
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = [
-    'mighty-shelf-44024.herokuapp.com',
+    'intense-dusk-68393.herokuapp.com',
     '127.0.0.1'
 ]
 
